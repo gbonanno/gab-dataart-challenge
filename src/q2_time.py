8b3cf886760cea97453a8ae3f939db2b393b7ea0
gbonanno <87429993+gbonanno@users.noreply.github.com>
2024-06-02 21:25:26 -0300

    Update q2_time.py
    Versión final con la lógica requerida para la función.

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,24 @@
 from typing import List, Tuple
+import json
+import pandas as pd
+import emoji
+from collections import Counter
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+  # Crear Counter para los emojis
+  emoji_counter = Counter()
+
+  # Abrir archivo
+  with open(file_path, 'r') as fp:
+    # Procesar cada línea del archivo
+    for line in fp:
+      line_json = json.loads(line)
+      content = line_json.get("content", "")
+
+      # Extraer los emojis
+      for char in content:
+        if char in emoji.EMOJI_DATA:
+          emoji_counter.update(char)
+
+  # Devolver el top 10 de emojis
+  return emoji_counter.most_common(10)
